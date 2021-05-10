0683dabf1bd257f908799c03943c0374d6201638
min-0 <dhe08133@naver.com>
2021-05-10 18:18:24 +0900

    상속

--- ch8_2.cpp
+++ ch8_2.cpp
@@ -5,38 +5,36 @@ using namespace std;
 class Circle{
 	int radius;
 public:
-	Circle(){ radius = 1; }
 	Circle(int radius = 0){ this->radius = radius; }
 	int getRadius(){ return radius; }
 	void setRadius(int radius){ this->radius = radius; }
 	double getArea(){ return 3.14*radius*radius; }
 };
-class NamedCircle : public Circle{
+class NameCircle : public Circle{
 	string name;
 public:
-	NamedCircle();
-	void setRN(int r, string name){
-		setRadius(r);
-		this->name = name;
+	void setRN(int R, string N){
+		setRadius(R);
+		this->name = N;
 	}
 	string getName(){ return name; }
 };
 int main(){
-	Circle a();
-	int size[5];
-	NamedCircle pizza[5];
-	int n, max;
 	string name;
-	cout << "5 개의 정수 반지름과 원의 이름을 입력하세요" << endl;
-
-	for (int i = 0; i < 5; i++){
-		cout << i + 1 << ">>";
-		cin >> n >> name;
-		pizza[i].setRN(n, name);
+	int radius, big;
+	double size[5];
+	NameCircle pizza[5];
+	cout << "5 개의 정수 반지름과 원의 이름을 입력하세요\n";
+	for (int i = 0; i<5; i++){
+		cout << i + 1 << ">> ";
+		cin >> radius >> name;
+		pizza[i].setRN(radius, name);
 		size[i] = pizza[i].getArea();
 	}
-	max = 0;
-	for (int i = 0; i < 5; i++){
-
+	big = 0;
+	for (int i = 0; i<4; i++){
+		if (size[i] < size[i + 1])
+			big = i + 1;
 	}
+	cout << "가장 면적이 큰 피자는 " << pizza[big].getName() << "입니다";
 }
